e4faf0ead84b36eb3f21d8ac79db479bfa13338d
Juveria Manzar <juveriamanzar29@gmail.com>
2020-06-09 01:25:32 +0530

    registering-user

--- models/users.js
+++ models/users.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 const crypto = require('crypto')
 
 const salt = crypto.randomBytes(64).toString('hex')
-console.log(salt)
 
-console.log('-------------------------------------------')
-const password = crypto.pbkdf2Sync("password1", salt, 1000, 64, "sha512").toString('hex')
-console.log(password)
 
 const userSchema = new mongoose.Schema({
     firstname: {
@@ -27,6 +23,14 @@ const userSchema = new mongoose.Schema({
 
 })
 
+userSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(64).toString('hex')
+    this.password = crypto.pbkdf2Sync(password, this.salt, 1000, 64, "sha512").toString("hex")
+}
+userSchema.methods.validatePassword = function(password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, "sha512").toString("hex")
+    return hash === this.password
+}
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const passport = require('passport');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
+
+router.post('/register', function({ body }, res, next) {
+    if (!Object.values(body).every((val) => val)) {
+        return res.send({ message: "All fields are required." })
+    }
+
+    if (body.password !== body.password_confirm) {
+        return res.send({ message: "Passwords don't match." })
+    }
+
+    const user = new User();
+    user.firstname = body.first_name.trim();
+    user.lastname = body.last_name.trim();
+    user.email = body.email.trim();
+    user.setPassword(body.password);
+
+    user.save((err, newUser) => {
+        if (err) {
+            res.status(404).json(err)
+        } else {
+            res.status(201).json({ message: "Created User", user: newUser })
+        }
+    })
+})
 
 module.exports = router;
