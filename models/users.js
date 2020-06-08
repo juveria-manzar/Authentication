6f7871c54b00b2f59507d0e1b2856358571b6c97
Juveria Manzar <juveriamanzar29@gmail.com>
2020-06-08 23:24:51 +0530

    model-structure-schema

---
+++ models/users.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: String,
+    salt: String
+
+})
